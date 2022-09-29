c315df452e2b3c8adeb0793ec0f0761d143e7b8b
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-09-29 19:22:53 +0300

    Create numbersNTo1.js

---
+++ For Loop - Lab/numbersNTo1.js
@@ -0,0 +1,12 @@
+function numbersNTo1 (input) {
+
+    let num = Number(input[0]);
+
+    for(i = num; i >= 1; i--) {
+        console.log(i);
+    }
+
+}
+// numbersNTo1(["2"])
+// numbersNTo1(["3"])
+numbersNTo1(["5"])
