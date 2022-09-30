7e1dce9743b84a386f96a8a98786a15ba0ca5436
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-09-30 12:06:06 +0300

    Create numbers1ToNWithStep3.js

---
+++ For Loop - Lab/numbers1ToNWithStep3.js
@@ -0,0 +1,12 @@
+function numbers1ToNWithStep3 (input) {
+
+    let num = Number(input[0]);
+
+    for(i = 1; i <= num; i +=3) {
+        console.log(i)
+    }
+
+}
+// numbers1ToNWithStep3(["10"])
+// numbers1ToNWithStep3(["7"])
+numbers1ToNWithStep3(["15"])
