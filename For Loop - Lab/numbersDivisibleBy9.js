8588a36749ce3baad5a00c4f7c6f0b5e11bc58d3
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-10-04 12:53:57 +0300

    Create numbersDivisibleBy9.js

---
+++ For Loop - Lab/numbersDivisibleBy9.js
@@ -0,0 +1,21 @@
+function numbersDivisibleBy9(input) {
+
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+
+    let sum = 0;
+    let buffer = "";
+
+    for (let i = start; i <= end; i++) {
+
+        if (i % 9 === 0) {
+            sum += i
+            buffer += i + "\n"
+        }
+    }
+
+    console.log("The sum: " + sum)
+    console.log(buffer)
+
+}
+numbersDivisibleBy9(["100", "200"])
