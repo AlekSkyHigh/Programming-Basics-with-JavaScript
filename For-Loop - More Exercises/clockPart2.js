ddb2aa64c4de2b779fb58ffee24b5cf04c331838
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-10-15 09:24:25 +0300

    Create clockPart2.js

---
+++ For-Loop - More Exercises/clockPart2.js
@@ -0,0 +1,12 @@
+function clockPart2() {
+
+    for (let h = 0; h <= 23; h++) {
+        for (let m = 0; m <= 59; m++) {
+            for(let s = 0; s <= 59; s++){
+                console.log(`${h} : ${m} : ${s}`);
+            }
+        }
+    }
+
+}
+clockPart2()
