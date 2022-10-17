9af44cd4dfde4220dc1e7fbb1b3dd7156983bac0
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-10-17 12:41:39 +0300

    Create sequence2kPlus1.js

---
+++ While Loop - Lab/sequence2kPlus1.js
@@ -0,0 +1,13 @@
+function sequence2kPlus1(input) {
+
+    let n = Number(input[0]);
+
+    let k = 1;
+
+    while (k <= n) {
+        console.log(k);
+        k = k * 2 + 1;
+    }
+
+}
+sequence2kPlus1(["31"])
