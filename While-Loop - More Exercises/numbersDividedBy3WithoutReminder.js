e4f20a750cf56d45885953f1d1ead547573e883f
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2022-10-25 09:32:54 +0300

    Create numbersDividedBy3WithoutReminder.js

---
+++ While-Loop - More Exercises/numbersDividedBy3WithoutReminder.js
@@ -0,0 +1,11 @@
+function numbersDividedBy3WithoutReminder() {
+
+    for (let index = 1; index <= 100; index++) {
+
+        if (index % 3 === 0) {
+            console.log(index);
+        }
+    }
+
+}
+numbersDividedBy3WithoutReminder()
